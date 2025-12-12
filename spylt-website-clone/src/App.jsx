449318c56100c8d8dcbf5be929acb00201dc450a
Obaapa Ama Ampaben-Kyereme <oaampaben@gmail.com>
2025-12-12 14:48:06 +0000

    update on 12/12/2025

---
+++ spylt-website-clone/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './components/NavBar.jsx'
+import HeroSection from './components/HeroSection.jsx'
+
+const App = () => {
+  return (
+    <main>
+      <NavBar />
+      <HeroSection />
+    </main>
+  )
+}
+
+export default App
